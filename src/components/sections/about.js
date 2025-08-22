a01fe23621809991d79eab9a985a82c1e0c69edf
gautiertonongbe <gautier.tonongbe@gmail.com>
2025-08-21 21:34:32 -0700

    Updated About.js to use avatar.jpg

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -208,7 +208,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/avatar.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
